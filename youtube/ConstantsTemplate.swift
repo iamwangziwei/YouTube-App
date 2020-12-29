7e04c88e455c63988a186786f5c3031c5ceef8c6
wangziwei <35870166+maoyiyiyi@users.noreply.github.com>
2020-12-29 11:54:18 -0500

    modified gitingnore to stop tracking constants.swift, add ComstantsTemplate.swift instead

---
+++ youtube/ConstantsTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstantsTemplate.swift
+//  youtube
+//
+//  Created by VV on 2020-12-29.
+//
+
+//add your own API_KEY and PLAYLIST_ID, then uncomment the code below
+
+import Foundation
+/*
+struct  Constants {
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL =  "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
